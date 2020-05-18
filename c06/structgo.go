1755370ebd4cd4f96738de453d339311a189a3ec
liuxinjia <liuxinjia@163.com>
2020-05-18 20:22:32 +0800

    struct

---
+++ c06/structgo.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type Books struct {
+	title  string
+	id     int
+	author string
+}
+
+func main() {
+	var book1 Books
+	book1.author = "tom"
+	book1.id = 123
+	book1.title = "cars"
+
+	fmt.Println("book1 title is " + book1.title)
+}
